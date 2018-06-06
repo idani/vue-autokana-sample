d7f417ebe7001c29fdc14ad476592945f4568f00
Shigehiro IDANI <idani@exp-t.com>
2018-06-06 12:32:43 +0900

    サンプル

---
+++ README.md
@@ -0,0 +1,36 @@
+# vue-autokana-sample
+
+vue.jsで自動でふりがなを入力するためのサンプルになります。
+
+利用ライブラリは、「terrierscript/historykana」になります。
+
+https://github.com/terrierscript/historykana
+
+``` bash
+npm i historykana
+```
+で導入します。
+
+サンプルでは、v-on:inputで文字入力イベントを拾っていますが、
+IMEが確定するまでは、v-modelで紐づけた値(サンプル内では、this.name)には値が入りません。
+このため取得したeventオブジェクトから、
+実際に入力されている値を取得しています。
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    漢字：<input type="text" v-model="name" v-on:input="update_furigana">
+    ふりがな：<input type="text" v-model="furigana">
+  </div>
+</template>
+
+<script>
+import historykana from 'historykana'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      name: '',
+      furigana: '',
+      history: []
+    }
+  },
+  methods: {
+    update_furigana (input) {
+      this.history.push(input.target.value)
+      this.furigana = historykana(this.history)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
